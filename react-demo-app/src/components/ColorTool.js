6daa997798dbccbba6627390b04314480df835f0
Eric Greene <eric@t4d.io>
2021-08-31 17:01:34 -0400

    add the add color demo

--- react-demo-app/src/components/ColorTool.js
+++ react-demo-app/src/components/ColorTool.js
@@ -1,6 +1,29 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export const ColorTool = ({ colors }) => {
+export const ColorTool = (props) => {
+
+  const [ colors, setColors ] = useState([...props.colors]);
+  const [ colorForm, setColorForm ] = useState({ name: '', hexcode: '' });
+
+  const change = (e) => {
+    setColorForm({
+      ...colorForm, // copy the properties from the orignal color form in to the new one
+      [ e.target.name ]: e.target.value,
+    });
+  };
+
+  const addColor = () => {
+    setColors([
+      ...colors,
+      {
+        ...colorForm,
+        id: Math.max(...colors.map(c => c.id), 0) + 1,
+      }
+    ]);
+  };
+
+  console.log(colorForm);
 
   return (
     <>
@@ -11,6 +34,17 @@ export const ColorTool = ({ colors }) => {
         {colors.map(color =>
           <li key={color.id}>{color.name}</li>)}
       </ul>
+      <form>
+        <label style={{display:'block'}}>
+          Color Name:
+          <input type="text" name="name" value={colorForm.name} onChange={change} />
+        </label>
+        <label style={{display:'block'}}>
+          Color Hexcode:
+          <input type="text" name="hexcode" value={colorForm.hexcode} onChange={change} />
+        </label>
+        <button type="button" onClick={addColor}>Add Color</button>
+      </form>
     </>
   );
 
