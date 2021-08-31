98daeb72df16b8b90204c72f47ec3a9783bb7971
Eric Greene <eric@t4d.io>
2021-08-31 15:02:58 -0400

    add solution to exercise 4

--- react-demo-app/src/components/CarTool.js
+++ react-demo-app/src/components/CarTool.js
@@ -1,10 +1,6 @@
+import PropTypes from 'prop-types';
 
-export const CarTool = () => {
-
-  const cars = [
-    { id: 1, make: 'Ford', model: 'Fusion Hybrid', year: 2018, color: 'blue', price: 45000 },
-    { id: 2, make: 'Tesla', model: 'S', year: 2019, color: 'red', price: 110000 },
-  ];
+export const CarTool = ({ cars }) => {
 
   return (
     <>
@@ -36,4 +32,19 @@ export const CarTool = () => {
     </>
   );
 
+};
+
+CarTool.defaultProps = {
+  cars: [],
+};
+
+CarTool.propTypes = {
+  cars: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    make: PropTypes.string.isRequired,
+    model: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
+    color: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  })),
 };
--- react-demo-app/src/index.js
+++ react-demo-app/src/index.js
@@ -9,7 +9,14 @@ const colorList = [
   { id: 3, name: 'blue', hexcode: '0000ff'},
 ];
 
+const carList = [
+  { id: 1, make: 'Ford', model: 'Fusion Hybrid', year: 2018, color: 'blue', price: 45000 },
+  { id: 2, make: 'Tesla', model: 'S', year: 2019, color: 'red', price: 110000 },
+];
+
+
+
 render(<>
   <ColorTool colors={colorList} />
-  <CarTool />
+  <CarTool cars={carList} />
 </>, document.querySelector("#root"));
