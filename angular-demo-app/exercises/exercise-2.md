1d6a17549984a8583a37e36c385b974ca71278b4
Eric Greene <eric@t4d.io>
2021-09-01 19:24:55 -0400

    add exercise 2

---
+++ angular-demo-app/exercises/exercise-2.md
@@ -0,0 +1,16 @@
+# Exercise 2
+
+1. Create an array of car objects and assign the array to a cars property on Car Home Component.
+
+Each car should have the following properties:
+
+id: number,
+make: string,
+model: string,
+year: number,
+color: string,
+price: number
+
+2. Display the array of car objects in a table in Car Home Component. Please use a table element and its related elements.
+
+3. Ensure it works!
--- angular-demo-app/src/app/app.module.ts
+++ angular-demo-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { ColorToolModule } from './color-tool/color-tool.module';
+import { CarToolModule } from './car-tool/car-tool.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     ColorToolModule,
+    CarToolModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
