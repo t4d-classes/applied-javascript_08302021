5fb48f3279dfca8efe3bf6d10b92025cd8222e27
Eric Greene <eric@t4d.io>
2021-09-03 17:12:16 -0400

    add rest router

--- node-full-stack-app/server-app/index.js
+++ node-full-stack-app/server-app/index.js
@@ -4,6 +4,9 @@ const express = require('express');
 require('dotenv').config();
 
 const { logger } = require('./logger');
+const { createRestRouter: createRestRouterMongo } = require('./routers/RestRouterMongo');
+const { createRestRouter: createRestRouterMemory } = require('./routers/RestRouterMemory');
+const { Car } = require('./models/car');
 
 const {
   DB_USER, DB_PASS, DB_CLUSTER_HOST,
@@ -22,11 +25,18 @@ process.on('exit', () => {
   global.appToolsConn.close();
 });
 
+const initialCars = [
+  { id: 1, make: 'Ford', model: 'Fusion', year: 2018, color: 'blue', price: 45000 },
+  { id: 2, make: 'Tesla', model: 'S', year: 2019, color: 'red', price: 110000 },
+];
+
 const app = express();
 
 app.use(express.json());
 
 // adding rest api middleware here a little later
+app.use('/api/carsmongo', createRestRouterMongo(Car));
+app.use('/api/carsmemory', createRestRouterMemory(initialCars));
 
 if (NODE_ENV === 'development') {
 
---
+++ node-full-stack-app/server-app/routers/RestRouterMemory.js
@@ -0,0 +1,63 @@
+const express = require('express');
+
+const { logger } = require('../logger');
+
+module.exports.createRestRouter = (initialItems) => {
+
+  let items = [ ...initialItems ];
+
+  const RestRouter = express.Router();
+
+  // Collection URL
+  RestRouter.route("/")
+    .get(async (req, res) => {
+      return [ ...items ];
+    })
+    .post(async (req, res) => {
+
+      const newItem = {
+        ...req.body,
+        _id: Math.max(...items.map(c => c._id), 0) + 1,
+      };
+
+      items = [
+        ...items,
+        newItem,
+      ];
+
+      res.json(newItem);
+    });
+
+  // Element/Member URL
+  RestRouter.route("/:id")
+    .get(async (req, res) => {
+      const item = items.find(i => i.id === parseInt(req.params.id, 10));
+
+      if (item === null) {
+        logger.error("unable to find it with id " + req.params.id);
+        res.sendStatus(404);
+      }
+
+      res.json(item);
+    })
+    .put(async (req, res) => {
+
+      const newItems = [...items];
+      const itemIndex = items.findIndex(c => c.id === req.params.id);
+      newItems[itemIndex] = {
+        ...req.body,
+      };
+
+      items = newItems;
+
+      res.sendStatus(204);
+
+    })
+    .delete(async (req, res) => {
+
+      items = items.filter(c => c.id !== parseInt(req.params.id, 10));
+      res.sendStatus(204);
+
+    });
+
+};
